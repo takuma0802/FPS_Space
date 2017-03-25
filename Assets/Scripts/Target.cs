58997b3c94e6d9de623782ca4d855c87a1d37fae
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2017-03-25 16:45:21 +0900

    create target life and broken motion and reload interval

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	[SerializeField] private GameObject target;
+	public int targetLife;
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = target.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Hit(){
+		targetLife--;
+		if (targetLife == 0)
+		{
+			//anim.SetBool ("IsBroken", true);
+			//Invoke ("Revival", 10f);
+		}
+	}
+
+	void Revival(){
+		anim.SetBool ("IsBroken", false);
+		targetLife = 5;
+	}
+
+}
